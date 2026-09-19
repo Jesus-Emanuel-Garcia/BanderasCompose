828328da8a016a36e514b88beebd0afe31c21e72
Jesus-Emanuel-Garcia <jesusmananuel2012@gmail.com>
2026-09-19 12:18:52 -0600

    Practica 01: Bandera de Mexico con Row y Box

--- app/src/main/java/com/example/banderascompose/MainActivity.kt
+++ app/src/main/java/com/example/banderascompose/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.banderascompose.ui.theme.BanderasComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +31,41 @@ class MainActivity : ComponentActivity() {
         setContent {
             BanderasComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+
+                    /*
+
+                Row(modifier = Modifier.padding(innerPadding).fillMaxSize().background(Color.Red))
+                {
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxHeight()
+                            .background(Color(0xFF006341)) // verde
                     )
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxHeight()
+                            .background(Color.White),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.descargar),
+                            contentDescription = "Escudo nacional",
+                            modifier = Modifier.size(60.dp)
+                        )
+                    }
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxHeight()
+                            .background(Color(0xFFCE1126)) // rojo
+                    )
+                }
+
+                 */
+
+
                 }
             }
         }
@@ -31,17 +73,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BanderaMexico(modifier: Modifier = Modifier) {
+    Row(modifier = modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .background(Color(0xFF006341)) // verde
+        )
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .background(Color.White),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.descargar),
+                contentDescription = "Escudo nacional",
+                modifier = Modifier.size(60.dp)
+            )
+        }
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .background(Color(0xFFCE1126)) // rojo
+        )
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    BanderasComposeTheme {
-        Greeting("Android")
+fun BanderaMexicoPreview() {
+    Surface {
+        BanderaMexico(modifier = Modifier.fillMaxSize())
     }
 }
