976c36b3a49ed3f6cc542a485e2e345903953d5b
Jesus-Emanuel-Garcia <jesusmananuel2012@gmail.com>
2026-09-20 11:15:12 -0600

    practica 05 españa

--- app/src/main/java/com/example/banderascompose/MainActivity.kt
+++ app/src/main/java/com/example/banderascompose/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.banderascompose.ui.theme.BanderasComposeTheme
 
@@ -20,10 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BanderasComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
@@ -31,17 +34,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BanderaEspaña(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
+
+        Box(Modifier.weight(1f).fillMaxWidth().background(Color(0xFFAA151B)))
+
+        Box(Modifier.weight(2f).fillMaxWidth().background(Color(0xFFF1BF00)))
+
+        Box(Modifier.weight(1f).fillMaxWidth().background(Color(0xFFAA151B)))
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    BanderasComposeTheme {
-        Greeting("Android")
+fun BanderaEspañaPreview() {
+    Surface {
+        BanderaEspaña(modifier = Modifier.fillMaxSize())
     }
 }
+
