da705c78ce99f3d048a386a1883df3aae68cc347
Jesus-Emanuel-Garcia <jesusmananuel2012@gmail.com>
2026-09-23 08:31:58 -0600

    bandera de usa separada por carpetas

--- app/src/main/java/com/example/banderascompose/Screens/BanderaScreen.kt
+++ app/src/main/java/com/example/banderascompose/Screens/BanderaScreen.kt
@@ -1,6 +1,7 @@
 package com.example.banderascompose.Screens
 
 import android.media.Image
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -9,13 +10,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -25,29 +32,101 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.banderascompose.R
 
-@Composable
-fun BanderaScreen(modifier: Modifier){
-    Row(modifier = modifier){
-        Column(modifier = Modifier.fillMaxHeight().weight(2f).background(colorResource(id = R.color.verde)), verticalArrangement = Arrangement.Center){
-            Text(text = stringResource(id = R.string.miNombre), fontSize = 60.sp)
-        }
-        Column(modifier = Modifier.fillMaxHeight().weight(2f).background(colorResource(id = R.color.white)), verticalArrangement = Arrangement.Center){
-            Image(painter = painterResource(id = R.drawable.descargar), contentDescription = "null")
-        }
-        Column(modifier = Modifier.fillMaxHeight().weight(2f).background(colorResource(id = R.color.rojo)), verticalArrangement = Arrangement.Center){
-            Box(modifier = Modifier.height(100.dp).width(100.dp).clip(CircleShape).background(colorResource(id = R.color.verde))){
-                Text("e", textAlign = TextAlign.Center)
 
+@Composable
+fun StarCanvas(modifier: Modifier = Modifier) {
+    Canvas(modifier = modifier) {
+
+        val path = Path()
+        val w = size.width
+        val h = size.height
+        val mx = w * 0.12f
+        val my = h * 0.12f
+
+        path.moveTo(w * 0.5f, my)
+        path.lineTo(w * 0.62f, h * 0.35f)
+        path.lineTo(w - mx, h * 0.35f)
+        path.lineTo(w * 0.70f, h * 0.58f)
+        path.lineTo(w * 0.80f, h - my)
+        path.lineTo(w * 0.5f, h * 0.75f)
+        path.lineTo(w * 0.20f, h - my)
+        path.lineTo(w * 0.30f, h * 0.58f)
+        path.lineTo(mx, h * 0.35f)
+        path.lineTo(w * 0.38f, h * 0.35f)
+        path.close()
+
+        drawPath(path, Color.White)
+    }
+}
+
+
+@Composable
+fun StarsUSA(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        repeat(11) { rowIndex ->
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                val starsInRow =
+                    if (rowIndex % 2 == 0) 5
+                    else 4
+
+                repeat(starsInRow) {
+                    StarCanvas(
+                        modifier = Modifier
+                            .size(28.dp)
+                            .padding(1.dp)
+                    )
+                }
             }
         }
     }
+}
+
+
+@Composable
+fun BanderaUSA(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
+        Column(Modifier.fillMaxSize()) {
+            repeat(13) { index ->
+                Box(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxWidth()
+                        .background(if (index % 2 == 0) Color(0xFFB22234) else Color.White)
+                )
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .fillMaxHeight(0.39f)
+                .background(Color(0xFF3C3B6E))
+        ){
+
+            StarsUSA(Modifier.fillMaxSize())
+
+        }
+    }
+
 
 }
 
+@Composable
+fun BanderaScreen(modifier: Modifier = Modifier) {
+    BanderaUSA(modifier = modifier)
+}
 @Preview(showBackground = true)
 @Composable
-fun BanderaScreenPreview(){
-    BanderaScreen(modifier = Modifier)
-
+fun BanderaUSAPreview() {
+        BanderaUSA(modifier = Modifier.fillMaxSize())
 }
 
